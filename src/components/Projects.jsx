ddd75769202825f37caacd5a43714f34269bd944
Alun Turner <alun.turner@googlemail.com>
2020-08-24 16:49:22 +0100

    added Project component with rough formatting

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,11 +2,39 @@ import React from "react";
 import Navbar from "./subcomponents/Navbar";
 import Buttonbar from "./subcomponents/Buttonbar";
 import AddProjectButton from "./subcomponents/AddProjectButton";
+import Project from "./subcomponents/Project";
+import { Box } from "@material-ui/core";
 
 function Projects() {
+  const projectArray = [];
+  const proj1 = {
+    name: "Castleford Drive",
+    number: 2412,
+    img:
+      "https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  };
+  const proj2 = {
+    name: "The Hawthorns",
+    number: 2413,
+    img:
+      "https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  };
+  const proj3 = {
+    name: "Buchan Drive",
+    number: 2414,
+    img:
+      "https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  };
+  projectArray.push(proj1, proj2, proj3);
+
   return (
     <>
       <Navbar text="Projects" />
+      <Box className="Projects-container">
+        {projectArray.map((project) => {
+          return <Project key={project.number} {...project} />;
+        })}
+      </Box>
       <Buttonbar>
         <AddProjectButton />
       </Buttonbar>
--- src/components/subcomponents/Project.jsx
+++ src/components/subcomponents/Project.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import {
+  Card,
+  Typography,
+  CardActionArea,
+  CardContent,
+} from "@material-ui/core";
 
-function Project() {
-  return <h1>This is a project</h1>;
+function Project({ name, number, img }) {
+  return (
+    <Card style={{ width: "80%", margin: "8px 0px" }}>
+      <CardActionArea>
+        <CardContent>
+          <img src={img} alt="a building" style={{ height: "64px" }} />
+          <Typography>{number}</Typography>
+          <Typography>{name}</Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
 
 export default Project;
