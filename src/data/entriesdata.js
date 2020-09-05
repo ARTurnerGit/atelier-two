579a1691d46a80eafc34679a13152a2b55021750
Alun Turner <alun.turner@googlemail.com>
2020-09-05 15:24:01 +0100

    wrote dummy entriesdata

---
+++ src/data/entriesdata.js
@@ -0,0 +1,26 @@
+const entriesArray = [];
+const entry1 = {
+  entry_img:
+    "https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  entry_heading: "The first photo",
+  entry_text:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud",
+};
+const entry2 = {
+  entry_img:
+    "https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  entry_heading: "Second snap",
+  entry_text:
+    "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in",
+};
+const entry3 = {
+  entry_img:
+    "https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  entry_heading: "Third",
+  entry_text:
+    "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+};
+
+entriesArray.push(entry1, entry2, entry3);
+
+export default entriesArray;
