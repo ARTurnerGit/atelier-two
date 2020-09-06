d317c31a001402e8f4e0fe278b876b23fb18f8d2
Alun Turner <alun.turner@googlemail.com>
2020-09-06 09:13:04 +0100

    complete accordion Entry.jsx implementation

--- src/components/subcomponents/Entry.jsx
+++ src/components/subcomponents/Entry.jsx
@@ -1,32 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Card,
-  CardActionArea,
-  CardContent,
   Typography,
   Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  AccordionActions,
+  TextField,
+  Button,
 } from "@material-ui/core";
-import { navigate } from "@reach/router";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Visit({ entry_id, entry_img, entry_heading, entry_text }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newEntryText, setNewEntryText] = useState(entry_text);
+
+  const handleCancel = () => {
+    setNewEntryText(entry_text);
+    setIsEditing(false);
+  };
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+  const handleEntry = (e) => {
+    setNewEntryText(e.target.value);
+  };
+
   return (
-    <>
-      <Accordion></Accordion>
-      <Card
-        style={{ width: "80%", margin: "8px 0px" }}
-        onClick={() => {
-          navigate();
-        }}
-      >
-        <CardActionArea>
-          <CardContent>
-            <img src={entry_img} alt="a building" style={{ height: "64px" }} />
-            <Typography>{entry_heading}</Typography>
-            <Typography>{entry_text}</Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </>
+    <Accordion
+      defaultExpanded={false}
+      style={{ width: "80%", margin: "8px 0px" }}
+    >
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <img src={entry_img} alt="a building" style={{ height: "64px" }} />
+        <Typography>{entry_heading}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        {isEditing ? (
+          <TextField
+            multiline
+            variant="outlined"
+            label="Editing"
+            rows={4}
+            value={newEntryText}
+            onChange={handleEntry}
+          />
+        ) : (
+          <Typography>{newEntryText}</Typography>
+        )}
+      </AccordionDetails>
+      <AccordionActions>
+        {isEditing ? (
+          <>
+            <Button onClick={handleCancel}>Cancel</Button>
+            <Button variant="contained" onClick={handleSave}>
+              Save
+            </Button>
+          </>
+        ) : (
+          <Button variant="contained" color="primary" onClick={handleEdit}>
+            Edit
+          </Button>
+        )}
+      </AccordionActions>
+    </Accordion>
   );
 }
 
