435365ec5d8701525c7a43b80e8ed5250ddbe795
Alun Turner <alun.turner@googlemail.com>
2020-09-06 08:30:59 +0100

    creating the entry accordion

--- src/components/subcomponents/Entry.jsx
+++ src/components/subcomponents/Entry.jsx
@@ -4,25 +4,29 @@ import {
   CardActionArea,
   CardContent,
   Typography,
+  Accordion,
 } from "@material-ui/core";
 import { navigate } from "@reach/router";
 
 function Visit({ entry_id, entry_img, entry_heading, entry_text }) {
   return (
-    <Card
-      style={{ width: "80%", margin: "8px 0px" }}
-      onClick={() => {
-        navigate();
-      }}
-    >
-      <CardActionArea>
-        <CardContent>
-          <img src={entry_img} alt="a building" style={{ height: "64px" }} />
-          <Typography>{entry_heading}</Typography>
-          <Typography>{entry_text}</Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      <Accordion></Accordion>
+      <Card
+        style={{ width: "80%", margin: "8px 0px" }}
+        onClick={() => {
+          navigate();
+        }}
+      >
+        <CardActionArea>
+          <CardContent>
+            <img src={entry_img} alt="a building" style={{ height: "64px" }} />
+            <Typography>{entry_heading}</Typography>
+            <Typography>{entry_text}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </>
   );
 }
 
