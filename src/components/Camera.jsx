c16a3041f24fcdc674f5f14c8921f826f01d41b1
Alun Turner <alun.turner@googlemail.com>
2020-09-07 09:02:24 +0100

    hooked up the ref for the video feed

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Navbar from "./subcomponents/Navbar";
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, Typography } from "@material-ui/core";
 import Buttonbar from "./subcomponents/Buttonbar";
 
 function Camera({ user_id, project_id, visit_id }) {
@@ -9,14 +9,15 @@ function Camera({ user_id, project_id, visit_id }) {
   let height = width;
 
   const [error, setError] = useState(null);
-  const [viewport, setViewport] = useState(null);
+  // const [viewport, setViewport] = useState(null);
+  const userFeed = useRef(null);
 
   useEffect(() => {
     function hasGetUserMedia() {
       return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
     }
     if (hasGetUserMedia) {
-      setViewport("Here is your video feed");
+      // setViewport("Here is your video feed");
     } else {
       setError("Not possible to use a camera on this device");
     }
@@ -33,7 +34,11 @@ function Camera({ user_id, project_id, visit_id }) {
             margin: "1vh 0",
           }}
         >
-          {error ? error : viewport}
+          {error ? (
+            <Typography>{error}</Typography>
+          ) : (
+            <video ref={userFeed} autoPlay></video>
+          )}
         </Box>
         <Button variant="outlined">Take the photo</Button>
       </Box>
